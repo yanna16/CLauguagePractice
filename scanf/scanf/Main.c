5f760ecf4f9ef5ea6d8f35b203bf510782caa05d
yanna16 <nana_0@163.com>
2015-08-15 23:31:31 +0800

    整理代码！

--- scanf/scanf/Main.c
+++ scanf/scanf/Main.c
@@ -1,11 +1,10 @@
-#define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
 
 int power(int a, int b)
 {
-    int c = a;
+    int c = 1;
 
-    for (int i = 1; i < b; i++)
+    for (int i = 0; i < b; i++)
     {
         c = c * a;
     }
@@ -15,57 +14,55 @@ int power(int a, int b)
 
 int main()
 {
-    int a, b, c;
-    char d;
+    int a = 0, b = 0, c = 0;
+    char d = 0;
 
     while (1)
     {
         printf("请输入一个正整数(输入0退出)：");
-        scanf("%d", &a);
+        scanf_s("%d", &a);
         if (a == 0)
+        {
             return 0;
+        }
 
         printf("请再输入一个正整数(输入0退出)：");
-        scanf("%d", &b);
+        scanf_s("%d", &b);
         if (b == 0)
+        {
             return 0;
+        }
 
         getchar();
         printf("请输入运算符（+ - * / %% ^）:");
-        scanf("%c", &d);
+        scanf_s("%c", &d, 1);
 
         if (d == '+') 
         {
             c = a + b;
-            printf("%d%c%d=%d\n", a, d, b, c);
         }
-        else if (d == '-') {
+        else if (d == '-')
+        {
             c = a - b;
-            printf("%d%c%d=%d\n", a, d, b, c);
         }
         else if (d == '*')
         {
             c = a * b;
-            printf("%d%c%d=%d\n", a, d, b, c);
         }
         else if (d == '/')
         {
             c = a / b;
-            printf("%d%c%d=%d\n", a, d, b, c);
         }
         else if (d == '%')
         {
             c = a % b;
-            printf("%d%c%d=%d\n", a, d, b, c);
         }
         else if (d == '^')
         {
             c = power(a, b);
-            printf("%d%c%d=%d\n", a, d, b, c);
         }
-        else {
-            printf("输入的字符非法！");
-        }
+        printf("%d%c%d=%d\n", a, d, b, c);
     }
+
     return 0;
 }
